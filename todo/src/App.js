6655870cd1dfeb8a1549588fb88986d3ace01d14
Dil Sombayeva <dilyasom@gmail.com>
2019-07-18 14:36:21 -0700

    add login button

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,25 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+ state = {};
+
+ hundleLogin = () => {};
+
+ render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+    <header className="App-header">
+     <img src={logo} className="App-logo" alt="logo" />
+     <div>Please, login to see the content</div>
+     <button onClick={this.hundleLogin}>Login</button>
+    </header>
+   </div>
   );
+ }
 }
 
 export default App;
