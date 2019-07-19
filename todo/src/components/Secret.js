f02d0489b18062a6d043baafd9d1c984bb839ba6
Dil Sombayeva <dilyasom@gmail.com>
2019-07-19 13:40:46 -0700

    change Link

--- todo/src/components/Secret.js
+++ todo/src/components/Secret.js
@@ -4,10 +4,11 @@ import { Route, Link } from 'react-router-dom';
 class Secret extends React.Component {
  state = {};
  render() {
+  //   console.log('props in secret: ', this.props);
   return (
    <div>
     <h1>Secret!!!</h1>
-    <Link to="/">Login</Link>
+    <Link to="/">Home</Link>
    </div>
   );
  }
