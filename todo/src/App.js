2a1dc38c15a0d94fd7bc926ef986e32200bd3a5f
Dil Sombayeva <dilyasom@gmail.com>
2019-07-18 18:58:52 -0700

    import Switch to render exact routes

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Secret from './components/Secret';
 
 class App extends React.Component {
  state = {};
