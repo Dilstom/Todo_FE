acfae5ef2902caab424d93dd5675508df63d20ee
Dil Sombayeva <dilyasom@gmail.com>
2019-07-21 13:23:27 -0700

    display each note from fake API

--- todo/src/components/NotesList/NotesList.js
+++ todo/src/components/NotesList/NotesList.js
@@ -7,172 +7,18 @@ class NotesList extends React.Component {
   this.state = {};
  }
  render() {
+  console.log('props in NotesList: ', this.props);
   return (
-   <div className="displayFlex">
-    <div className="card" style={{ width: '18rem' }}>
-     <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-       Some quick example text to build on the card title and make up the bulk
-       of the card's content.
-      </p>
-      <a href="#" className="card-link">
-       Card link
-      </a>
-      <a href="#" className="card-link">
-       Another link
-      </a>
-     </div>
-    </div>
-    <div className="card" style={{ width: '18rem' }}>
-     <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-       Some quick example text to build on the card title and make up the bulk
-       of the card's content.
-      </p>
-      <a href="#" className="card-link">
-       Card link
-      </a>
-      <a href="#" className="card-link">
-       Another link
-      </a>
-     </div>
-    </div>
-    <div className="card" style={{ width: '18rem' }}>
-     <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-       Some quick example text to build on the card title and make up the bulk
-       of the card's content.
-      </p>
-      <a href="#" className="card-link">
-       Card link
-      </a>
-      <a href="#" className="card-link">
-       Another link
-      </a>
-     </div>
-    </div>
-    <div className="card" style={{ width: '18rem' }}>
-     <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-       Some quick example text to build on the card title and make up the bulk
-       of the card's content.
-      </p>
-      <a href="#" className="card-link">
-       Card link
-      </a>
-      <a href="#" className="card-link">
-       Another link
-      </a>
-     </div>
-    </div>
-    <div className="card" style={{ width: '18rem' }}>
-     <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-       Some quick example text to build on the card title and make up the bulk
-       of the card's content.
-      </p>
-      <a href="#" className="card-link">
-       Card link
-      </a>
-      <a href="#" className="card-link">
-       Another link
-      </a>
-     </div>
-    </div>
-    <div className="card" style={{ width: '18rem' }}>
-     <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-       Some quick example text to build on the card title and make up the bulk
-       of the card's content.
-      </p>
-      <a href="#" className="card-link">
-       Card link
-      </a>
-      <a href="#" className="card-link">
-       Another link
-      </a>
-     </div>
-    </div>
-    <div className="card" style={{ width: '18rem' }}>
-     <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-       Some quick example text to build on the card title and make up the bulk
-       of the card's content.
-      </p>
-      <a href="#" className="card-link">
-       Card link
-      </a>
-      <a href="#" className="card-link">
-       Another link
-      </a>
-     </div>
-    </div>
-    <div className="card" style={{ width: '18rem' }}>
-     <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-       Some quick example text to build on the card title and make up the bulk
-       of the card's content.
-      </p>
-      <a href="#" className="card-link">
-       Card link
-      </a>
-      <a href="#" className="card-link">
-       Another link
-      </a>
-     </div>
-    </div>
-    <div className="card" style={{ width: '18rem' }}>
-     <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-       Some quick example text to build on the card title and make up the bulk
-       of the card's content.
-      </p>
-      <a href="#" className="card-link">
-       Card link
-      </a>
-      <a href="#" className="card-link">
-       Another link
-      </a>
-     </div>
-    </div>
-    <div className="card" style={{ width: '18rem' }}>
-     <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-       Some quick example text to build on the card title and make up the bulk
-       of the card's content.
-      </p>
-      <a href="#" className="card-link">
-       Card link
-      </a>
-      <a href="#" className="card-link">
-       Another link
-      </a>
-     </div>
-    </div>
-    <div className="card" style={{ width: '18rem' }}>
-     <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-       Some quick example text to build on the card title and make up the bulk
-       of the card's content.
-      </p>
-      <a href="#" className="card-link">
-       Card link
-      </a>
-      <a href="#" className="card-link">
-       Another link
-      </a>
-     </div>
+   <div className="card" style={{ width: '18rem' }}>
+    <div className="card-body">
+     <h5 className="card-title">{this.props.item.name}</h5>
+     <p className="card-text">{this.props.item.body}</p>
+     <a href="#" className="card-link">
+      Card link
+     </a>
+     <a href="#" className="card-link">
+      Another link
+     </a>
     </div>
    </div>
   );
