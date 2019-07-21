06c3fab697ea012a2e31a5d4534a82862a60336e
Dil Sombayeva <dilyasom@gmail.com>
2019-07-21 12:25:45 -0700

    add CreateNote folder

---
+++ todo/src/components/CreateNote/CreateNote.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+class CreateNote extends React.Component {
+ constructor(props) {
+  super(props);
+  this.state = {};
+ }
+ render() {
+  return <div>CreateNote</div>;
+ }
+}
+
+export default CreateNote;
