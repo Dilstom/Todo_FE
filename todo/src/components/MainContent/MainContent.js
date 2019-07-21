0b6b7010548d77050a31d93ddabaf15a9dee788e
Dil Sombayeva <dilyasom@gmail.com>
2019-07-21 13:23:03 -0700

    se fake API to display notes

--- todo/src/components/MainContent/MainContent.js
+++ todo/src/components/MainContent/MainContent.js
@@ -5,14 +5,28 @@ import NotesList from '../NotesList/NotesList';
 class MainContent extends React.Component {
  constructor(props) {
   super(props);
-  this.state = {};
+  this.state = {
+   notes: [],
+  };
  }
+ componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/comments/')
+   .then(response => response.json())
+   .then(json => {
+    let top10 = json.slice(0, 10);
+    this.setState({ notes: top10 });
+   });
+ }
+
  render() {
+  //   console.log('state: ', this.state.notes);
   return (
    <div>
     <h4 className="headerN">Your Notes: </h4>
-    <div className="cardWrapper">
-     <NotesList />
+    <div className="cardWrapper displayFlex">
+     {this.state.notes.map(item => {
+      return <NotesList key={item.id} item={item} />;
+     })}
     </div>
    </div>
   );
