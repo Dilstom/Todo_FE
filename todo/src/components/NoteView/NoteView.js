b950f453ff7183df9c0537a3c75561404915f004
Dil Sombayeva <dilyasom@gmail.com>
2019-07-22 12:38:16 -0700

    add wireframe for NoteView component

--- todo/src/components/NoteView/NoteView.js
+++ todo/src/components/NoteView/NoteView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './index.css';
+import NavBar from '../NavBar/NavBar';
 
 class NoteView extends React.Component {
  constructor(props) {
@@ -7,7 +8,29 @@ class NoteView extends React.Component {
   this.state = {};
  }
  render() {
-  return <div>NoteView</div>;
+  return (
+   <div>
+    <NavBar logout={this.props.auth.logout} />
+    <div className="cardWrapperNV">
+     <div class="card" style={{ maxWidth: '48rem' }}>
+      <div class="card-body">
+       <h5 class="card-title">Card title</h5>
+       <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+       <p class="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+       </p>
+       <a href="#" class="card-link">
+        Card link
+       </a>
+       <a href="#" class="card-link">
+        Another link
+       </a>
+      </div>
+     </div>
+    </div>
+   </div>
+  );
  }
 }
 
