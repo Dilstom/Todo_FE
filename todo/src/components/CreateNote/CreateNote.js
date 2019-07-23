ff4bd0748b3bdfa471eedc4f4555255bf7d51828
Dil Sombayeva <dilyasom@gmail.com>
2019-07-23 16:31:27 -0700

    implement post req

--- todo/src/components/CreateNote/CreateNote.js
+++ todo/src/components/CreateNote/CreateNote.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import './index.css';
 import NavBar from '../NavBar/NavBar';
+import axios from 'axios';
 
 class CreateNote extends React.Component {
  constructor(props) {
   super(props);
-  this.state = {};
+  this.state = {
+   title: '',
+   body: '',
+   status: '',
+  };
  }
+
+ handleChange = e => {
+  this.setState({ [e.target.name]: e.target.value });
+ };
+
+ handleSubmit = e => {
+  e.preventDefault();
+  const newObj = {
+   title: this.state.title,
+   body: this.state.body,
+  };
+
+  axios
+   .post('https://jsonplaceholder.typicode.com/posts/', newObj)
+   .then(res => {
+    this.setState({ status: res.statusText });
+   })
+   .catch(err => console.log(err));
+ };
+
  render() {
   return (
    <div>
@@ -15,30 +40,30 @@ class CreateNote extends React.Component {
      <div className="card" style={{ maxWidth: '40rem' }}>
       <div className="card-body">
        <h4 className="headerSave">Create New Note: </h4>
-       <form
-        className="createForm"
-        // onSubmit={}
-       >
-        <div class="form-group">
+       <form className="createForm" onSubmit={this.handleSubmit}>
+        <div className="form-group">
          {/* <label for="exampleFormControlInput1">Note Title</label> */}
          <input
-          type="email"
-          class="form-control"
-          id="exampleFormControlInput1"
+          type="text"
+          className="form-control"
+          id="exampleFormControlTextarea1"
           placeholder="Note Title"
+          onChange={this.handleChange}
          />
         </div>
 
-        <div class="form-group">
+        <div className="form-group">
          {/* <label for="exampleFormControlTextarea1">Note Content</label> */}
          <textarea
-          class="form-control"
+          className="form-control"
           placeholder="Note Content"
           id="exampleFormControlTextarea1"
           rows="10"
+          onChange={this.handleChange}
          />
         </div>
         <button className="saveBtn">Save</button>
+        <div>{this.state.status ? this.state.status + '!' : null}</div>
        </form>
       </div>
      </div>
