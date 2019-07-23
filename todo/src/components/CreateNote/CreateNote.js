e3c2a1a7519bc25e89aae1405ca0892fd246fe96
Dil Sombayeva <dilyasom@gmail.com>
2019-07-23 10:31:12 -0700

    add and style CreateNote component

--- todo/src/components/CreateNote/CreateNote.js
+++ todo/src/components/CreateNote/CreateNote.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './index.css';
+import NavBar from '../NavBar/NavBar';
 
 class CreateNote extends React.Component {
  constructor(props) {
@@ -7,7 +8,43 @@ class CreateNote extends React.Component {
   this.state = {};
  }
  render() {
-  return <div>CreateNote</div>;
+  return (
+   <div>
+    <NavBar logout={this.props.auth.logout} />
+    <div className="cardWrapperNV">
+     <div className="card" style={{ maxWidth: '40rem' }}>
+      <div className="card-body">
+       <h4 className="headerSave">Create New Note: </h4>
+       <form
+        className="createForm"
+        // onSubmit={}
+       >
+        <div class="form-group">
+         {/* <label for="exampleFormControlInput1">Note Title</label> */}
+         <input
+          type="email"
+          class="form-control"
+          id="exampleFormControlInput1"
+          placeholder="Note Title"
+         />
+        </div>
+
+        <div class="form-group">
+         {/* <label for="exampleFormControlTextarea1">Note Content</label> */}
+         <textarea
+          class="form-control"
+          placeholder="Note Content"
+          id="exampleFormControlTextarea1"
+          rows="10"
+         />
+        </div>
+        <button className="saveBtn">Save</button>
+       </form>
+      </div>
+     </div>
+    </div>
+   </div>
+  );
  }
 }
 
