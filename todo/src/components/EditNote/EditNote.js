01eaf56a95652d4bd1cfcd7a786271b29a45c232
Dil Sombayeva <dilyasom@gmail.com>
2019-07-23 10:33:13 -0700

    start with EditNote component

--- todo/src/components/EditNote/EditNote.js
+++ todo/src/components/EditNote/EditNote.js
@@ -7,7 +7,43 @@ class EditNote extends React.Component {
   this.state = {};
  }
  render() {
-  return <div>editNote</div>;
+  return (
+   <div>
+    <NavBar logout={this.props.auth.logout} />
+    <div className="cardWrapperNV">
+     <div className="card" style={{ maxWidth: '40rem' }}>
+      <div className="card-body">
+       <h4 className="headerSave">Edit Note: </h4>
+       <form
+        className="createForm"
+        // onSubmit={}
+       >
+        <div class="form-group">
+         {/* <label for="exampleFormControlInput1">Note Title</label> */}
+         <input
+          type="email"
+          class="form-control"
+          id="exampleFormControlInput1"
+          placeholder="Note Title"
+         />
+        </div>
+
+        <div class="form-group">
+         {/* <label for="exampleFormControlTextarea1">Note Content</label> */}
+         <textarea
+          class="form-control"
+          placeholder="Note Content"
+          id="exampleFormControlTextarea1"
+          rows="10"
+         />
+        </div>
+        <button className="saveBtn">Save</button>
+       </form>
+      </div>
+     </div>
+    </div>
+   </div>
+  );
  }
 }
 
