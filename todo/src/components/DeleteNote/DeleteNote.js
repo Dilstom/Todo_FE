6f3cdc75f3575c69e56f899834b9fb17cc670583
Dil Sombayeva <dilyasom@gmail.com>
2019-07-24 08:34:15 -0700

    implement modal for delete

--- todo/src/components/DeleteNote/DeleteNote.js
+++ todo/src/components/DeleteNote/DeleteNote.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import './index.css';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class DeleteNote extends React.Component {
  constructor(props) {
   super(props);
   this.state = {};
  }
+
  render() {
-  return <div>DeleteNote</div>;
+  return (
+   <div>
+    <Modal
+     isOpen={this.props.modal}
+     toggle={this.props.toggle}
+     className={this.props.className}
+    >
+     {/* <ModalHeader toggle={this.props.toggle}>Modal title</ModalHeader> */}
+     <ModalBody>Are you sure you want to delete this note?</ModalBody>
+     <ModalFooter>
+      <Button color="#C30505" onClick={this.props.handleDelete}>
+       Delete
+      </Button>{' '}
+      <Button color="#202374" onClick={this.props.toggle}>
+       No
+      </Button>
+     </ModalFooter>
+    </Modal>
+   </div>
+  );
  }
 }
 
