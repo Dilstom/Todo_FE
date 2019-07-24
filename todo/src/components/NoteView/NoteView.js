5a7c4ca23ccc739adab61443977da43a585f69eb
Dil Sombayeva <dilyasom@gmail.com>
2019-07-24 08:35:16 -0700

    implement delete

--- todo/src/components/NoteView/NoteView.js
+++ todo/src/components/NoteView/NoteView.js
@@ -1,23 +1,57 @@
 import React from 'react';
 import './index.css';
 import NavBar from '../NavBar/NavBar';
+import { Link } from 'react-router-dom';
+import DeleteNote from '../DeleteNote/DeleteNote';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import axios from 'axios';
 
 class NoteView extends React.Component {
  constructor(props) {
   super(props);
-  this.state = {};
+  this.state = {
+   modal: false,
+   status: '',
+  };
+  this.toggle = this.toggle.bind(this);
+ }
+
+ handleDelete = e => {
+  const id = this.props.location.state.id;
+
+  axios
+   .delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+   .then(res => {
+    console.log('res in handleDelete: ', res);
+    // this.setState({ modal: !this.state.modal, status: 'You ' });
+    this.props.history.push('/secret');
+   })
+   .catch(err => {
+    console.log('err: ', err);
+   });
+ };
+
+ toggle() {
+  this.setState(prevState => ({
+   modal: !prevState.modal,
+  }));
  }
 
  render() {
-  //   console.log('props in noteView render: ', this.props);
+  console.log('props in noteView render: ', this.props);
   return (
    <div>
     <NavBar logout={this.props.auth.logout} />
     <div className="cardWrapperNV">
      <div className="card" style={{ maxWidth: '40rem' }}>
       <div className="displayFlexRight">
-       <a className="mr-4">edit</a>
-       <a>delete</a>
+       <Link
+        to={{ pathname: '/edit', state: this.props.location.state }}
+        className="editLink mr-4"
+       >
+        edit
+       </Link>
+       <a onClick={this.toggle}>delete</a>
       </div>
       <div className="card-body">
        <h5 className="card-title">{this.props.location.state.title}</h5>
@@ -32,6 +66,12 @@ class NoteView extends React.Component {
       </div>
      </div>
     </div>
+    <DeleteNote
+     modal={this.state.modal}
+     toggle={this.toggle}
+     className={this.props.className}
+     handleDelete={this.handleDelete}
+    />
    </div>
   );
  }
