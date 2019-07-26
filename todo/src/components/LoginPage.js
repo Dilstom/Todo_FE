c0541a0dde614e126f129005cc98a115aa99cf05
Dil Sombayeva <dilyasom@gmail.com>
2019-07-25 22:10:42 -0700

    align content in Login Page

--- todo/src/components/LoginPage.js
+++ todo/src/components/LoginPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './loginPage.css';
 import { Link } from 'react-router-dom';
 
 class LoginPage extends React.Component {
@@ -9,19 +10,25 @@ class LoginPage extends React.Component {
  render() {
   console.log(this.props.auth.getProfile());
   return (
-   <div>
-    <div>Hello {this.props.auth.getProfile().given_name || 'User'}!</div>
-    <p>
-     Welcome to: <Link to="/secret">Notes</Link>
-    </p>
-    {/* display the login button only when the user is unauthenticated*/}
-    {!this.props.auth.isAuthenticated() && (
-     <div>
-      <h6>Please, login first to see the content</h6>
-      {/* we have auth object so we can use its property */}
-      <button onClick={this.props.auth.login}>Login</button>
-     </div>
-    )}
+   <div className="loginPage">
+    {/* <div>Hello {this.props.auth.getProfile().given_name || 'User'}!</div> */}
+    <div className="loginPageContent">
+     <p>
+      Welcome to <Link to="/secret">Notes</Link>
+     </p>
+     {/* display the login button only when the user is unauthenticated*/}
+     {!this.props.auth.isAuthenticated() ? (
+      <div>
+       <h6>Please, login first to see the content</h6>
+       {/* we have auth object so we can use its property */}
+       <button onClick={this.props.auth.login}>Login</button>
+      </div>
+     ) : (
+      <button className="getStarted">
+       <Link to="/secret">Get started</Link>
+      </button>
+     )}
+    </div>
    </div>
   );
  }
